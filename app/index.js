6584291be7b9519cfbfb477f7cb79dc8dad361ab
Benedict Taguinod <benedict.a.taguinod@gmail.com>
2023-01-08 12:34:00 -0800

    add tilt data

--- app/index.js
+++ app/index.js
@@ -1,1 +1,59 @@
-console.log('Hello world!');
+console.log('app init');
+import { Accelerometer } from "accelerometer";
+import * as document from "document";
+
+let ACTIVE = false;
+
+function sendMessage(data) {
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    // Send the data to peer as a message
+    messaging.peerSocket.send(data);
+  }
+}
+let accelerometer
+if (Accelerometer) {
+  console.log("This device has an Accelerometer!");
+  accelerometer = new Accelerometer({ frequency: 10 });
+  const pitch = document.getElementById("pitch");
+  const roll = document.getElementById("roll");
+  function dist(a, b) {
+    let result = Math.sqrt((Math.pow(a,2) + Math.pow(b,2)));
+    return result;
+  }
+  accelerometer.addEventListener("reading", () => {
+    let ax = accelerometer.x;
+    let ay = accelerometer.y;
+    let az = accelerometer.z;
+    const G = 0.806;
+    // from https://stackoverflow.com/questions/3755059/3d-accelerometer-calculate-the-orientation
+    let pitchVal = Math.atan2(ax, dist(ay, az))*180/Math.PI;
+    let miu = 0.01
+    let sign = az > 0 ? 1 : -1
+    let rollVal = Math.atan2(ay, sign*dist(ax*miu, az))*180/Math.PI;
+    pitch.text = "pitch: " +  pitchVal.toString().slice(0, 5) + "deg";
+    roll.text = "roll: " + rollVal.toString().slice(0, 5) + "deg";
+    sendMessage({
+      pitch: pitchVal, 
+      roll: rollVal
+    })
+  });
+} else {
+   console.log("This device does NOT have an Accelerometer!");
+}
+
+import * as messaging from "messaging";
+
+messaging.peerSocket.addEventListener("error", (err) => {
+  console.error(`Connection error: ${err.code} - ${err.message}`);
+});
+
+const myButton = document.getElementById("activate-button");
+
+myButton.addEventListener("click", (evt) => {
+  ACTIVE = !ACTIVE;
+  if (ACTIVE == false) {
+    accelerometer.stop();
+  } else {
+    accelerometer.start();
+  }
+})
--- companion/index.js
+++ companion/index.js
@@ -1,1 +1,33 @@
-console.log('Hello world!');
+console.log("companion init");
+
+const wsUri = "wss://socketsbay.com/wss/v2/1/demo/";
+const websocket = new WebSocket(wsUri);
+
+websocket.addEventListener("open", onOpen);
+websocket.addEventListener("close", onClose);
+websocket.addEventListener("message", onMessage);
+websocket.addEventListener("error", onError);
+
+function onOpen(evt) {
+  let message = "open test!"
+  console.log("CONNECTED");
+  websocket.send(message);
+}
+
+function onClose(evt) {
+   console.log("DISCONNECTED");
+}
+
+function onMessage(evt) {
+   console.log(`MESSAGE: ${evt.data}`);
+}
+
+function onError(evt) {
+   console.error(`ERROR: ${evt.data}`);
+}
+
+import * as messaging from "messaging";
+
+messaging.peerSocket.addEventListener("message", (evt) => {
+  console.error(JSON.stringify(evt.data));
+});
